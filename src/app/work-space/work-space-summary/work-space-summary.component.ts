383e370c663f2f520693f600f4e01ab9ddbebb6d
rrajiv07 <rajiv.r@bahwancybertek.com>
2021-02-08 14:03:52 +0530

    Changes

--- src/app/work-space/work-space-summary/work-space-summary.component.ts
+++ src/app/work-space/work-space-summary/work-space-summary.component.ts
@@ -11,23 +11,23 @@ import { HttpHeaders } from '@angular/common/http';
   styleUrls: ['./work-space-summary.component.css']
 })
 export class WorkSpaceSummaryComponent implements OnInit {
-  milestoneDtl:any;
-  POCDetails: any=[];
+  milestoneDtl: any;
+  POCDetails: any = [];
   boardId: string;
   wsPocId: string;
   micrositeId: string;
-  wsPocName:any;
+  wsPocName: any;
   token = localStorage.getItem('tempCurrentUserToken');
   header = {
     headers: new HttpHeaders()
       .set('Authorization', `Bearer ${this.token}`)
   };
   teamDtl = [];
-  totalCount:any=0;
-  openCount:any=0;
-  closedCount:any=0;
-  topReviewerName:any;
-  mostReviewedPhase:any;
+  totalCount: any = 0;
+  openCount: any = 0;
+  closedCount: any = 0;
+  topReviewerName: any;
+  mostReviewedPhase: any;
 
   constructor(private actRoute: ActivatedRoute,
     private workSpaceSummary: WorkSpaceSummaryService,
@@ -58,10 +58,24 @@ export class WorkSpaceSummaryComponent implements OnInit {
         });
   }
   onClosePOC() {
+    const param = { micrositeId: this.micrositeId, id: this.wsPocId };
+    this.workSpaceSummary.closePOC(param, this.header)
+      .pipe(first())
+      .subscribe(
+        (data: any) => {
+          if (data.result_status.toUpperCase() === 'SUCCESS') {
+            this.commonService.successMessage(data['result_msg']);
+            this.POCDetails = data.result_data;
+            return;
+          }
+          this.commonService.failureMessage(data['result_msg']);
+        },
+        error => {
+        });
   }
 
   onManagePOC() {
-   this.route.navigateByUrl('/workspace/view/' + this.wsPocId + '/' + this.wsPocName + '/edit-workspace')
+    this.route.navigateByUrl('/workspace/view/' + this.wsPocId + '/' + this.wsPocName + '/edit-workspace')
     //this.route.navigateByUrl('/workspace/view/' + this.wsPocId + '/' + this.POCDetails.pocName + '/4424/phase/information-architecture' );
   }
 
@@ -72,10 +86,9 @@ export class WorkSpaceSummaryComponent implements OnInit {
       .subscribe(
         (data: any) => {
           if (data.result_status.toUpperCase() == 'SUCCESS') {
-            if (data.result_data !=undefined)
-            {
-            this.commonService.setRole(data.result_data.userRoleToWorkspace);
-            this.POCDetails = data.result_data;
+            if (data.result_data != undefined) {
+              this.commonService.setRole(data.result_data.userRoleToWorkspace);
+              this.POCDetails = data.result_data;
             }
             return;
           }
@@ -95,8 +108,7 @@ export class WorkSpaceSummaryComponent implements OnInit {
       });
     this.micrositeId = JSON.parse(localStorage.getItem('micrositeId'));
   }
-  getDashboard()
-  {
+  getDashboard() {
     const reqdata = {
       "micrositeId": parseInt(this.micrositeId),
       "workspaceId": parseInt(this.wsPocId)
@@ -105,27 +117,27 @@ export class WorkSpaceSummaryComponent implements OnInit {
       .pipe(first())
       .subscribe(
         (data: any) => {
-          if (data.result_status.toUpperCase() == "SUCCESS" && data.result_data !=null) {
+          if (data.result_status.toUpperCase() == "SUCCESS" && data.result_data != null) {
             //this.commentsReceived = data.result_data;
 
-            this.totalCount =data.result_data.reviewTotalCount;
-            this.openCount =data.result_data.reviewOpenCount;
-            this.closedCount=data.result_data.reviewClosedCount;
-            this.topReviewerName=data.result_data.topReviewerName;
-            this.mostReviewedPhase=data.result_data.mostReviewedPhase;
+            this.totalCount = data.result_data.reviewTotalCount;
+            this.openCount = data.result_data.reviewOpenCount;
+            this.closedCount = data.result_data.reviewClosedCount;
+            this.topReviewerName = data.result_data.topReviewerName;
+            this.mostReviewedPhase = data.result_data.mostReviewedPhase;
             return;
           }
         },
         error => {
         });
   }
-  getMilestones(){
+  getMilestones() {
     const param = { micrositeId: this.micrositeId, workspaceId: this.wsPocId };
     this.workSpaceSummary.getMilestones(param, this.header)
       .pipe(first())
       .subscribe(
         (data: any) => {
-          if (data.result_status.toUpperCase() === 'SUCCESS' && data.result_data !=null) {
+          if (data.result_status.toUpperCase() === 'SUCCESS' && data.result_data != null) {
             this.milestoneDtl = data.result_data;
             return;
           }
